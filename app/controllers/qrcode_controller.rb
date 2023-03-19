ae57db95c9d94b7645272a46bb634b86ec7dcd99
quentinchampenois <quentin.champenois7@gmail.com>
2023-03-19 20:14:51 +0100

    Transform QRCode from SVG to PNG

--- app/controllers/qrcode_controller.rb
+++ app/controllers/qrcode_controller.rb
@@ -5,7 +5,7 @@ class QrcodeController < ApplicationController
   def generate
     @query = params["query"] || ""
 
-    @svg = qrcode_to_svg(@query)
+    @svg = qrcode_to_png(@query).to_data_url
   end
 
   private
@@ -19,6 +19,23 @@ class QrcodeController < ApplicationController
       module_size: 11,
       standalone: true,
       use_path: true
+    )
+  end
+
+  def qrcode_to_png(query)
+    qrcode = RQRCode::QRCode.new(query)
+
+    qrcode.as_png(
+      bit_depth: 1,
+      border_modules: 4,
+      color_mode: ChunkyPNG::COLOR_GRAYSCALE,
+      color: "black",
+      file: nil,
+      fill: "white",
+      module_px_size: 6,
+      resize_exactly_to: false,
+      resize_gte_to: false,
+      size: 350
     )
   end
 end
